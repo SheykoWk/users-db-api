9ff2feaf41c721f86ea5d5ea8e9f5df0d8fbb48b
Sh3yk0  (Sahid Kick) <sheyko.wk@gmail.com>
2023-03-29 20:05:34 -0600

    add: update and delete user services

--- src/users/users.services.js
+++ src/users/users.services.js
@@ -37,6 +37,37 @@ const postNewUser = (req, res) => {
         })
 }
 
+const patchUser = (req, res) => {
+
+    const id = Number(req.params.id) 
+    const userObj = req.body 
+
+    userControllers.updateUser(id, userObj)
+        .then(data => {
+            if(!data) {
+                return res.status(404).json({message: 'Invalid ID'})
+            }
+            res.status(200).json(data)
+        })
+        .catch(err => {
+            res.status(400).json({message: 'Bad request', err})
+        })
+}
+
+const deleteUser = (req, res) => {
+    const id = req.params.id 
+    userControllers.deleteUser(id)
+        .then(data => {
+            if(!data) {
+                return res.status(404).json({message: 'Invalid ID'})
+            }
+            res.status(204).json()
+        })
+        .catch(err => {
+            res.status(400).json({message: 'Bad request', err})
+        })
+}
+
 module.exports = {
     getAllUsers,
     getUserById,
